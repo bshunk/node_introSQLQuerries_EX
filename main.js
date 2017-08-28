2ee2402084f11365de6f94e2dde5862e665b837e
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-08-28 14:52:02 -0500

    finish exercise number four

--- main.js
+++ main.js
@@ -6,8 +6,14 @@
 //            VALUES (29, 'Rebelution', 1999)
 
 // 3. Using the INSERT statement, add one, or more, albums by your artist to the Album table.
-//    answer: 
-// 
+//    answer: INSERT INTO album (albumId, title, releaseDate, albumLength, label, artistId, genreId)
+//    VALUES (25, 'Bright Side Of Life', 2009, 3456, '87 Music', 29, 1)
+
+// 4. Using the INSERT statement, add some songs that are on that album to the Song table.
+//    answer: INSERT INTO song (songId, title, songLength, releaseDate, genreId, artistId, albumId)
+//    VALUES (23, 'Moonlight', 123, '2009', 1, 29, 25)
+
+
 
 
 
