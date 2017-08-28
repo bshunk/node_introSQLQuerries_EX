7495f72bfcbeab883540a910eca6c55914ea12e6
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-08-28 14:42:39 -0500

    complete exerciises one and two in Node exercise module

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 // 1. Query all of the entries in the Genre table
 //   answer: 'SELECT * FROM genre'
 
+// 2. Using the INSERT statement, add one of your favorite artists to the Artist table.
+//    answer: INSERT INTO artist (artistId, artistName, yearEstablished)
+//            VALUES (29, 'Rebelution', 1999)
+
 // 
+
+
+
+
+
