2ef1cdba6e495d73759c752452fc8951bb7f2cd9
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-08-28 15:10:14 -0500

    work on the left join exercise

--- main.js
+++ main.js
@@ -13,7 +13,13 @@
 //    answer: INSERT INTO song (songId, title, songLength, releaseDate, genreId, artistId, albumId)
 //    VALUES (23, 'Moonlight', 123, '2009', 1, 29, 25)
 
+// 5. Write a SELECT query that provides the song titles, album title, and artist name for all of the data you just entered in. Use the 
+//    LEFT JOIN keyword sequence to connect the tables, and the WHERE keyword to filter the results to the album and artist you added. 
+//    Here is some more info on joins that might help.
 
+//    Reminder: Direction of join matters. Try the following statements and see the difference in results.
+//    (SELECT a.Title, s.Title FROM Album a LEFT JOIN Song s ON s.AlbumId = a.AlbumId;)
+//    (SELECT a.Title, s.Title FROM Song s LEFT JOIN Album a ON s.AlbumId = a.AlbumId;)
 
 
 
